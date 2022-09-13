0ae3866b664c3e64a854d0b3205634ad765f0f3e
herdypad <herdypad@gmail.com>
2022-09-13 11:25:00 +0700

    hay

--- plugins/aplikasi.js
+++ plugins/aplikasi.js
@@ -1,10 +1,10 @@
 exports.run = {
-   usage: ['menu', 'help', 'bot'],
+   usage: ['cekaplikasi'],
    async: async (m, {
       client,
       isPrefix
    }) => {
-      client.reply(m.chat, `Hai ${m.pushName}  \nBot ini hanya bisa di akses lewat Aplikasi \n\n ketik \n #cekaplikasi`, m)
+      client.reply(m.chat, `Hai ${m.pushName}  \nbisa di Akses di \nhttps://play.google.com/store/apps/details?id=com.indobot.botwastikerapp`, m)
    },
    error: false
 }
--- plugins/sticker.js
+++ plugins/sticker.js
@@ -15,13 +15,13 @@ exports.run = {
             if (/video/.test(type)) {
                if (q.seconds > 10) return client.reply(m.chat, Func.texted('bold', `Durasi video maksimal 10 detik.`), m)
                return await client.sendSticker(m.chat, img, m, {
-                  packname: 'No Bot Wa',
-                  author: '6285158870125'
+                  packname: 'Klik Liat Lebih Banyak',
+                  author: 'Buat Bikin Sticker'
                })
             } else if (/image/.test(type)) {
                return await client.sendSticker(m.chat, img, m, {
-                  packname: 'No Bot Wa',
-                  author: '6285158870125'
+                  packname: 'Buat Bikin Sticker',
+                  author: 'Klik Liat Lebih Banyak'
                })
             }
          } else {
@@ -31,16 +31,16 @@ exports.run = {
                let img = await q.download()
                if (!img) return client.reply(m.chat, global.status.wrong, m)
                return await client.sendSticker(m.chat, img, m, {
-                  packname: 'No Bot Wa',
-                  author: '6285158870125'
+                  packname: 'Buat Bikin Sticker',
+                  author: 'Klik Liat Lebih Banyak'
                })
             } else if (/video/.test(mime)) {
                if ((q.msg || q).seconds > 10) return client.reply(m.chat, Func.texted('bold', `Durasi video maksimal 10 detik.`), m)
                let img = await q.download()
                if (!img) return client.reply(m.chat, global.status.wrong, m)
                return await client.sendSticker(m.chat, img, m, {
-                  packname: 'No Bot Wa',
-                  author: '6285158870125'
+                  packname: 'Buat Bikin Sticker',
+                  author: 'Klik Liat Lebih Banyak'
                })
             } else client.reply(m.chat, Func.texted('bold', `Gambar Atau Video Nya mana`), m)
          }
