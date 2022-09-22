7022181720fc60386605d4d364e0ffd0e5f27476
herdypad <herdypad@gmail.com>
2022-09-22 15:17:36 +0700

    saya

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,13 +1,13 @@
-exports.run = {
-   usage: ['menu', 'help', 'bot'],
-   async: async (m, {
-      client,
-      isPrefix
-   }) => {
-      client.reply(m.chat, `Hai ${m.pushName}  \nMenu Bot baru ini \n1. stiker \n2. stikergif \n\nkirim Foto Atau Video dengan Caption diatas yah \n\nMenu Lainya Nyusul Yah`, m)
-   },
-   error: false
-}
+// exports.run = {
+//    usage: ['menu', 'help', 'bot'],
+//    async: async (m, {
+//       client,
+//       isPrefix
+//    }) => {
+//       client.reply(m.chat, `Hai ${m.pushName}  \nMenu Bot baru ini \n1. stiker \n2. stikergif \n\nkirim Foto Atau Video dengan Caption diatas yah \n\nMenu Lainya Nyusul Yah`, m)
+//    },
+//    error: false
+// }
 
 
-//  bisa di Akses di \nhttps://play.google.com/store/apps/details?id=com.indobot.botwastikerapp
+// //  bisa di Akses di \nhttps://play.google.com/store/apps/details?id=com.indobot.botwastikerapp
